26b1780c73930e3c478ec714df0a740d635294f3
lefreire <letfreirefigueiredo@gmail.com>
2016-12-06 23:11:40 -0200

    Correcting the syntax

--- modelagemBD/modeloFisico.sql
+++ modelagemBD/modeloFisico.sql
@@ -1,88 +1,88 @@
--- Geração de Modelo físico
+-- Geraï¿½ï¿½o de Modelo fï¿½sico
 -- Sql ANSI 2003 - brModelo.
 
 
 
 CREATE TABLE Bairro (
-	NomeBairro VARCHAR() PRIMARY KEY,
+	NomeBairro VARCHAR(50) PRIMARY KEY,
 	Latitude FLOAT,
 	Longitude FLOAT
-)
+);
 
 CREATE TABLE QuantBairro2013 (
-	NomeBairro VARCHAR() PRIMARY KEY,
+	NomeBairro VARCHAR(50) PRIMARY KEY,
 	Total INT,
 	FOREIGN KEY(NomeBairro) REFERENCES Bairro (NomeBairro)
-)
+);
 
 CREATE TABLE QuantBairro2012 (
-	NomeBairro VARCHAR() PRIMARY KEY,
+	NomeBairro VARCHAR(50) PRIMARY KEY,
 	Total INT,
 	FOREIGN KEY(NomeBairro) REFERENCES Bairro (NomeBairro)
-)
+);
 
 CREATE TABLE CRE (
-	Codigo VARCHAR() PRIMARY KEY,
-	NomeIniciail VARCHAR(),
-	UltimoNome VARCHAR(),
+	Codigo VARCHAR(2) PRIMARY KEY,
+	NomeIniciail VARCHAR(20),
+	UltimoNome VARCHAR(20),
 	imagemSatelite BLOB
-)
+);
 
 CREATE TABLE Telefone (
-	Codigo_conselho VARCHAR(),
-	Telefone VARCHAR(),
+	Codigo_conselho VARCHAR(2),
+	Telefone VARCHAR(9),
 	PRIMARY KEY(Codigo_conselho,Telefone),
 	FOREIGN KEY(Codigo_conselho) REFERENCES CRE (Codigo) ON DELETE CASCADE
-)
+);
 
 CREATE TABLE Serie (
 	Codigo INT PRIMARY KEY,
-	Descrição VARCHAR()
-)
+	Descricao VARCHAR(20)
+);
 
 CREATE TABLE Escola (
-	Designacao VARCHAR() PRIMARY KEY,
-	Nome VARCHAR(),
+	Designacao VARCHAR(10) PRIMARY KEY,
+	Nome VARCHAR(70),
 	Latitude FLOAT,
 	Longitude FLOAT,
-	NomeBairro VARCHAR(),
+	NomeBairro VARCHAR(50),
 	FOREIGN KEY(NomeBairro) REFERENCES Bairro (NomeBairro)
-)
+);
 
 CREATE TABLE Disciplina (
-	Nome VARCHAR(),
-	Sigla VARCHAR() PRIMARY KEY,
-	Descricao VARCHAR()
-)
+	Nome VARCHAR(25),
+	Sigla VARCHAR(10) PRIMARY KEY,
+	Descricao VARCHAR(50)
+);
 
 CREATE TABLE DisciplinaEscola (
 	numeroProfessores INT,
-	Sigla VARCHAR(),
-	Designacao VARCHAR(),
+	Sigla VARCHAR(10),
+	Designacao VARCHAR(10),
 	PRIMARY KEY(Sigla,Designacao),
 	FOREIGN KEY(Sigla) REFERENCES Disciplina (Sigla),
 	FOREIGN KEY(Designacao) REFERENCES Escola (Designacao)
-)
+);
 
 CREATE TABLE EscolaSerie (
 	Codigo INT,
-	Designacao VARCHAR(),
+	Designacao VARCHAR(10),
 	PRIMARY KEY(Codigo,Designacao),
 	FOREIGN KEY(Codigo) REFERENCES Serie (Codigo),
 	FOREIGN KEY(Designacao) REFERENCES Escola (Designacao)
-)
+);
 
 CREATE TABLE FrequenciaAprovados (
 	TotalAvaliados INT,
 	TotalAprovados INT,
 	TotalReprovados INT,
 	PercAprovados FLOAT,
-	Ano Letivo INT,
-	PercFrequencia VARCHAR(),
+	AnoLetivo INT,
+	PercFrequencia FLOAT(2,1),
 	Codigo INT,
-	CodigoCRE VARCHAR(),
-	PRIMARY KEY(Ano Letivo,Codigo,CodigoCRE),
+	CodigoCRE VARCHAR(2),
+	PRIMARY KEY(AnoLetivo,Codigo,CodigoCRE),
 	FOREIGN KEY(Codigo) REFERENCES Serie (Codigo),
 	FOREIGN KEY(CodigoCRE) REFERENCES CRE (Codigo)
-)
+);
 
